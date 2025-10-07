295b0a8c6da68b8263eab41b66bc213672fae257
oarchner <oliver.archner@uni-bayreuth.de>
2025-10-07 12:38:51 +0200

    Dropped query by last record id, use dataSinceTime only

--- csbridge.py
+++ csbridge.py
@@ -10,11 +10,12 @@ Created on 06.07.2022
 import socket
 from time import sleep
 from bayeosgatewayclient import BayEOSWriter, BayEOSSender
-from configparser import ConfigParser
+import json
 import tempfile
 import csv
 from datetime import datetime, timedelta
 import os
+from os import path
 import sys
 import logging
 from campbell.logger import Logger
@@ -30,31 +31,33 @@ LOGGER_NAN = "NAN"
 
 def main():
     conf = readConfig()        
-    pa = conf.get(SEC_BRIDGE,'path')           
+    pa = path.join(tempfile.gettempdir(),"csbridge")  
     writer = BayEOSWriter(pa, max_chunk=10000)              
     sender = BayEOSSender(pa,'csbridge', conf.get(SEC_GATEWAY,'url'), conf.get(SEC_GATEWAY,'password'), conf.get(SEC_GATEWAY,'username'))
     sender.start()
     
     sleep_time = conf.getint(SEC_BRIDGE,'sleep_time')
 
-    lr_map = {}
-
+    lt_cache = {}
     while True:  
         for logger in getLoggerList(conf):                                  
                 host, tz, tables = logger                                                                          
                 for table in tables.split(','):                   
                     origin = host + '/' + table                         
                     try:
-                        lrec = None
-                        if origin in lr_map:
-                            lrec = lr_map[origin]
-                        lr_map[origin] = fetchAndSaveData(writer,host,table,tz,lrec,origin,conf)                        
+                        lt = None
+                        if origin in lt_cache:
+                            lt = lt_cache[origin]
+                        lt_cache[origin] = fetchAndSaveData(writer,host,table,tz,lt,origin,conf)                        
                     except Exception as err:
                         logging.error("Communication error:{0}".format(str(err)))                                            
                     else: 
                         logging.info("Import of {0} finished.".format(origin))        
         logging.info("Going to sleep for {0} secs ...".format(sleep_time))
         sleep(sleep_time)
+
+ 
+
 
 def getLoggerList(conf):
     loggers = []
@@ -63,23 +66,24 @@ def getLoggerList(conf):
             loggers.append((conf.get(section,'host'),conf.get(section,'tz'),conf.get(section,'tables')))
     return loggers           
     
-def fetchAndSaveData(writer,host,table,tz,lastRec,origin,conf, re=0):
+def fetchAndSaveData(writer,host,table,tz,lt,origin,conf,re=0):
     """ Fetch and save records from Campbell logger 
     @param host: logger hostname
     @param table: logger table name
     @param tz: time zone of logger datetime values as text
-    @param lastRec: last record id as integer
+    @param lt: last record time as datetime or None
     @returns lastRec: as integer
     """   
     logging.info("Fetch data from {}".format(origin))
     logger = Logger('http://'+ host)     
-    if lastRec is None:        
+    if lt is None:        
         logging.info("Get most recent data")
         data = logger.dataMostRecent("dl:{}".format(table),conf.getint(SEC_BRIDGE,'most_recent'))        
     else:        
-        logging.info("Get data since record:{}".format(lastRec))
-        data = logger.dataSinceRecord("dl:{}".format(table),lastRec)   
+        logging.info("Get data since time:{}".format(lt))
+        data = logger.dataSinceTime("dl:{}".format(table),lt)   
     n = 0
+    logging.info("Data length: {}".format(len(data)))
     for rec in data['data']: 
             values = {}            
             for i, field in enumerate(data['head']['fields']):               
@@ -91,18 +95,17 @@ def fetchAndSaveData(writer,host,table,tz,lastRec,origin,conf, re=0):
             if len(values)>0:
                 dt = datetime.strptime(rec['time'],'%Y-%m-%dT%H:%M:%S')
                 dt = dt.replace(tzinfo=ZoneInfo(tz))
-                writer.save(values=values,value_type=0x61,timestamp=dt.timestamp(),origin=origin) 
-            lastRec= rec['no']
-            lastTime = rec['time']
+                writer.save(values=values,value_type=0x61,timestamp=dt.timestamp(),origin=origin)             
+                lt = dt
             n = n + 1
-    logging.info("{} records fetched. Last record:{} Last time:{}".format(n, lastRec, lastTime))
+    logging.info("{} records fetched. Last time:{}".format(n, lt))
     if 'more' in data and data['more'] and re < 10:
         sleep(5) 
         logging.info("Attempt {} to get more data".format(re+1))
         re = re + 1
-        lastRec = fetchAndSaveData(writer,host,table,tz,lastRec,origin,conf,re)
+        lt = fetchAndSaveData(writer,host,table,tz,lt,origin,conf,re)
     writer.flush()    
-    return lastRec
+    return lt
     
     
 def readConfig():
@@ -115,16 +118,7 @@ def readConfig():
         conf.read('csbridge.conf')
     else:
         conf.read('/etc/csbridge.conf')
-    
-    # Overwrite default values
-    if not conf.has_option(SEC_BRIDGE, 'name'):
-        conf.set(SEC_BRIDGE,'name',socket.gethostname())    
-    if not conf.has_option(SEC_BRIDGE, 'path'):
-        conf.set(SEC_BRIDGE, 'path', os.path.join(tempfile.gettempdir(),conf.get(SEC_BRIDGE,'name')))    
-    if not conf.has_option(SEC_BRIDGE, 'most_recent'):
-        conf.set(SEC_BRIDGE, 'most_recent',str(50000))     
-    if not conf.has_option(SEC_BRIDGE, 'sleep_time'):
-        conf.set(SEC_BRIDGE, 'sleep_time',str(600))         
+       
     return conf
         
 if __name__ == '__main__':
